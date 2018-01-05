895a186553b7b9deeae7c2f343d4d658bdb5ddce
relson816 <relson.batayola@gmail.com>
2018-01-05 23:25:34 +0800

    Declare directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: '<h1 highlight>{{title}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
